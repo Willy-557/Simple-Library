e4f2c977086919e4cd658f9f69aace71d51d5247
Willy-557 <williamfsetiadi@gmail.com>
2026-01-09 07:49:06 +0700

    Make the menu of library

---
+++ FileSimpleLibrary.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.HashMap;
+
+class Buku {
+
+}
+
+public class FileSimpleLibrary {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        HashMap <String, Buku> penyimpananBuku = new HashMap<>();
+
+        while (true) {
+            System.out.println("--- Library ---");
+            System.out.println("1. Tambah buku baru");
+            System.out.println("2. Lihat daftar buku");
+            System.out.println("3. Pinjam buku");
+            System.out.println("4. Kembalikan buku");
+            System.out.print(">>");
+            int opsi = scanner.nextInt();
+            
+        }
+    }
+}
