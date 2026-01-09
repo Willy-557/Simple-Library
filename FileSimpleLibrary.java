ca4414c13e57fd1c3cd5beca1332385f5f26db84
Willy-557 <williamfsetiadi@gmail.com>
2026-01-09 07:50:19 +0700

    Make the validation input menu

--- FileSimpleLibrary.java
+++ FileSimpleLibrary.java
@@ -16,9 +16,20 @@ public class FileSimpleLibrary {
             System.out.println("2. Lihat daftar buku");
             System.out.println("3. Pinjam buku");
             System.out.println("4. Kembalikan buku");
+            System.out.println("5. Keluar");
             System.out.print(">>");
             int opsi = scanner.nextInt();
-            
+
+            if (opsi == 5) {
+                System.out.println("Terimakasih!");
+                break;
+            }
+            else if (opsi > 5 || opsi < 1){
+                System.out.println("Invalid input!");
+            }
+            else {
+                
+            }
         }
     }
 }
